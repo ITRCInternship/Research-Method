b50115870c5328d9c4718466bf0701fa96023233
haniyeh <h2860445@gmail.com>
2025-07-30 20:20:32 +0430

    make changes

--- app/static/js/about.js
+++
@@ -1,9 +0,0 @@
-
-document.addEventListener("DOMContentLoaded", () => {
-  const members = document.querySelectorAll(".team-member");
-  members.forEach((member, index) => {
-    setTimeout(() => {
-      member.classList.add("visible");
-    }, index * 200); 
-  });
-});
