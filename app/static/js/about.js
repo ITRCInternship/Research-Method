a8f870ac2f39ce4689eaf687b9fecf6c9f603d5b
alireza parvaresh <parvvaresh@gmail.com>
2025-08-02 22:18:50 +0330

    fix about page

---
+++ app/static/js/about.js
@@ -0,0 +1,8 @@
+{/* <script>
+  document.querySelectorAll(".toggle-social").forEach(button => {
+    button.addEventListener("click", () => {
+      const icons = button.nextElementSibling;
+      icons.style.display = icons.style.display === "flex" ? "none" : "flex";
+    });
+  });
+</script> */}
